1cfac84ecc37e0b97decd599c07bbcfd0423e88e
bahromjon <abdulhayevbahromjon2002@gmail.com>
2023-02-13 12:46:56 +0500

    api

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const mainURL = axios.create({
-    baseURL: "http://localhost:8000"
+    baseURL: "https://elonbering-backend.onrender.com/"
 })
 
 export default mainURL;
